faf6ec0cb33139af0cc6c7250f3c8872e0cdc4fa
laiyongshan <850422483@qq.com>
2018-01-23 18:20:22 +0800

    s

--- base/src/main/java/com/lys/base/utils/router/RouterUtils.java
+++ base/src/main/java/com/lys/base/utils/router/RouterUtils.java
@@ -11,7 +11,6 @@ public class RouterUtils {
 
     public static void inject(Object obj){
         ARouter.getInstance().inject(obj);
-
     }
 
     /**
@@ -25,8 +24,6 @@ public class RouterUtils {
         return  ARouter.getInstance().build(path).navigation();
     }
 
-
-
     public void startActivityForResult(Activity activity,String path,int requestCode,String argKey,Object argValue) {
         ARouter.getInstance().build(path).
                 withObject(argKey, argValue).
@@ -37,7 +34,6 @@ public class RouterUtils {
 // 使用两个参数的navigation方法，可以获取单次跳转的结果
         //ARouter.getInstance().build("/test/1").navigation(this, new NavigationCallback() );
     }
-
 
 }
 
